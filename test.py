d477149ff446b0758c64c6e5b47b9eed00664342
Yicuo <dgfanicuo@gmail.com>
2017-08-23 21:44:39 +0800

    使用多进程

--- test.py
+++ test.py
@@ -11,3 +11,28 @@
 # test11
 # test12
 # test13
+from multiprocessing import Pool
+
+d = {}
+
+def f1(i):
+    global  d
+    d[i] = i
+    print(d)
+
+def process_way1(i):
+    workers = 10
+    with Pool(workers) as p:
+        r = p.map(f1, i)
+    return r
+
+def f2():
+    global d
+    g =  [1,2,3,4]
+    r = process_way1(g)
+    for i in r:
+        print(i)
+    print(d)
+
+if __name__ == '__main__':
+    f2()
